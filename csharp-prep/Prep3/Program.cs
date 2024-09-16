a67f1096973699383230d4d66b3b34344694cc06
gacunach <gabriela.acunach@gmail.com>
2024-09-16 20:39:11 -0300

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess;
+        do 
+        {
+            Console.Write("What's your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+                    if (number > guess)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else 
+            {
+                Console.WriteLine("Congratulations, you guessed the number!");
+            }
+        } while (number != guess);
+
+
+
     }
 }
