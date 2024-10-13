902ae66cc2ccc76ec06d69b03c5835916671e8f4
gacunach <136766421+gacunach@users.noreply.github.com>
2024-10-12 22:58:39 -0300

    fix in program

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,25 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-       public class Comment
-{
-    
-    public string CommenterName { get; set; }
-    public string CommentText { get; set; }
+        List<Video> videos = new List<Video>();
 
-   
-    public Comment(string commenterName, string commentText)
-    {
-        CommenterName = commenterName;
-        CommentText = commentText;
-    }
+        
+        Video video1 = new Video("Learning to Code", "CodeAcademy", 300);
+        video1.AddComment(new Comment("John", "Great tutorial!"));
+        video1.AddComment(new Comment("Jane", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Doe", "Can you make a video about loops?"));
 
-    
-    public void DisplayComment()
-    {
-        Console.WriteLine($"{CommenterName}: \"{CommentText}\"");
-    }
-}
+        Video video2 = new Video("Advanced C# Programming", "TechGuru", 600);
+        video2.AddComment(new Comment("Alice", "Loved the examples!"));
+        video2.AddComment(new Comment("Bob", "Could you explain delegates more?"));
+
+        Video video3 = new Video("Introduction to OOP", "LearnTech", 450);
+        video3.AddComment(new Comment("Sam", "OOP is fun!"));
+        video3.AddComment(new Comment("Paul", "Can you do an advanced tutorial?"));
+
+        
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
 
     }
 }
