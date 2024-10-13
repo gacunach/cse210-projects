b4bb17c3034299d35cbcaadeacbb6ccfea96fefe
gacunach <136766421+gacunach@users.noreply.github.com>
2024-10-12 23:06:01 -0300

    name change

--- foundation/Foundation1/Comment.cs
+++ foundation/Foundation1/Comment.cs
@@ -4,7 +4,7 @@ public class Comment
     public string CommenterName { get; set; }
     public string CommentText { get; set; }
 
-    // Constructor
+    
     public Comment(string commenterName, string commentText)
     {
         CommenterName = commenterName;
