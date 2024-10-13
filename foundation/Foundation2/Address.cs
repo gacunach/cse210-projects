65a96cb2b007a77832117d7a36727d5aa92cc9e9
gacunach <136766421+gacunach@users.noreply.github.com>
2024-10-12 23:14:44 -0300

    fix

--- foundation/Foundation2/Address.cs
+++ foundation/Foundation2/Address.cs
@@ -1,50 +1,50 @@
 public class Address
 {
     
-    private string streetAddress;
-    private string city;
-    private string stateOrProvince;
-    private string country;
+    private string _streetAddress;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
 
     
     public Address(string streetAddress, string city, string stateOrProvince, string country)
     {
-        this.streetAddress = streetAddress;
-        this.city = city;
-        this.stateOrProvince = stateOrProvince;
-        this.country = country;
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
     }
 
     
     public string GetStreetAddress()
     {
-        return streetAddress;
+        return _streetAddress;
     }
 
     public string GetCity()
     {
-        return city;
+        return _city;
     }
 
     public string GetStateOrProvince()
     {
-        return stateOrProvince;
+        return _stateOrProvince;
     }
 
     public string GetCountry()
     {
-        return country;
+        return _country;
     }
 
     
     public bool IsInUSA()
     {
-        return country.ToLower() == "usa";
+        return _country.ToLower() == "usa";
     }
 
     
     public string GetFullAddress()
     {
-        return $"{streetAddress}\n{city}, {stateOrProvince}\n{country}";
+        return $"{_streetAddress}\n{_city}, {_stateOrProvince}\n{_country}";
     }
 }
