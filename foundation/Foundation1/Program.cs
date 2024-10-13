07deda6ddc0e8c404f7215830edd857c66817c3e
gacunach <136766421+gacunach@users.noreply.github.com>
2024-10-12 22:25:10 -0300

    program

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+       public class Comment
+{
+    
+    public string CommenterName { get; set; }
+    public string CommentText { get; set; }
+
+   
+    public Comment(string commenterName, string commentText)
+    {
+        CommenterName = commenterName;
+        CommentText = commentText;
+    }
+
+    
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{CommenterName}: \"{CommentText}\"");
+    }
+}
+
     }
 }
